20f156951b6965c3523fb2aa76036d23890fb517
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-30 19:58:28 +0800

    >  compile NEMU ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:58:28 up 4 days, 23:28,  2 users,  load average: 0.13, 0.29, 0.16

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -212,7 +212,7 @@ word_t expr(char *e, bool *success) {
 
   for(int i=0;i<nr_token;i++)
   {
-      if (tokens[i].type == '*' && (i == 0 || tokens[i - 1].type == TK_NOTYPE || tokens[i-1].type=='*'||tokens[i-1].type=='/' || tokens[i-1].type=='*'||tokens[i-1].type=='/')) 
+      if (tokens[i].type == '*' && (i == 0 || tokens[i - 1].type == TK_NOTYPE || tokens[i-1].type=='+'||tokens[i-1].type=='-'||tokens[i-1].type=='*'||tokens[i-1].type=='/')) 
          tokens[i].type = TK_POT;
   }
   /* TODO: Insert codes to evaluate the expression. */
