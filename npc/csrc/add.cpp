cb0a26f2e4b08f81dbcc51b4ac5f3093b9c20611
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-03 19:33:25 +0800

    >  generate verilog ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:33:24 up  5:01,  4 users,  load average: 0.38, 0.43, 0.43

--- npc/csrc/add.cpp
+++ npc/csrc/add.cpp
@@ -9,6 +9,7 @@ vluint64_t sim_time = 0;
 
 int main(int argc, char** argv, char** env)
 {
+	srand(time(0));
 	VerilatedContext* contextp = new VerilatedContext;
 	contextp->commandArgs(argc, argv);
 	Vadd* top = new Vadd{contextp};
@@ -21,14 +22,14 @@ int main(int argc, char** argv, char** env)
 	    
 	while (sim_time<MAX_SIM_TIME) 
 	{ 
-		long long value1=rand();
-		long long value2=rand();
+		long int value1=rand();
+		long int value2=rand();
 		top->io_value1=value1;
 		top->io_value2=value2;
 		top->eval();
 		m_trace->dump(sim_time);
 		sim_time++;
-		printf("value1=%lld value1=%lld top->output=%lld\n",value1,value2,top->io_output);
+		printf("value1=%ld value2=%ld top->output=%ld\n",value1,value2,top->io_output);
 		assert(top->io_output==value1+value2);
 	}
 
