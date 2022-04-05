6accd28ceacb5add53fab07c93a59833d8bcfa42
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-06 00:26:22 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:26:22 up 2 days,  9:54,  4 users,  load average: 0.60, 0.37, 0.20

--- npc/vsrc/EbreakBox.v
+++ npc/vsrc/EbreakBox.v
@@ -2,7 +2,10 @@
  import "DPI-C" function int ebreak();
  module EbreakBox (ebreak_in);
  input ebreak_in;
- wire [31:0] tmp;
- assign tmp=ebreak_in?ebreak():0;
+ always@(*)
+ begin
+   if(ebreak_in)
+     ebreak();
+ end
  endmodule
     
