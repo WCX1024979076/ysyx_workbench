5bb51e5a54b901460590c78d53581b471620991e
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-05 12:55:03 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:55:03 up 1 day, 22:23,  5 users,  load average: 0.34, 0.35, 0.34

---
+++ npc/obj_dir/VMain__Syms.h
@@ -0,0 +1,39 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Symbol table internal header
+//
+// Internal details; most calling programs do not need this header,
+// unless using verilator public meta comments.
+
+#ifndef VERILATED_VMAIN__SYMS_H_
+#define VERILATED_VMAIN__SYMS_H_  // guard
+
+#include "verilated.h"
+
+// INCLUDE MODEL CLASS
+
+#include "VMain.h"
+
+// INCLUDE MODULE CLASSES
+#include "VMain___024root.h"
+
+// SYMS CLASS (contains all model state)
+class VMain__Syms final : public VerilatedSyms {
+  public:
+    // INTERNAL STATE
+    VMain* const __Vm_modelp;
+    bool __Vm_activity = false;  ///< Used by trace routines to determine change occurred
+    uint32_t __Vm_baseCode = 0;  ///< Used by trace routines when tracing multiple models
+    bool __Vm_didInit = false;
+
+    // MODULE INSTANCE STATE
+    VMain___024root                TOP;
+
+    // CONSTRUCTORS
+    VMain__Syms(VerilatedContext* contextp, const char* namep, VMain* modelp);
+    ~VMain__Syms();
+
+    // METHODS
+    const char* name() { return TOP.name(); }
+} VL_ATTR_ALIGNED(VL_CACHE_LINE_BYTES);
+
+#endif  // guard
