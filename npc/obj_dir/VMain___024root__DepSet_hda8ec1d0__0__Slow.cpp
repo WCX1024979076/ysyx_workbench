4455ec3c6fd8ec72ae571b63f51d5902362d3920
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-06 00:28:32 +0800

    >  generate verilog ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:28:32 up 2 days,  9:56,  4 users,  load average: 0.28, 0.32, 0.20

--- npc/obj_dir/VMain___024root__DepSet_hda8ec1d0__0__Slow.cpp
+++ npc/obj_dir/VMain___024root__DepSet_hda8ec1d0__0__Slow.cpp
@@ -14,11 +14,12 @@ VL_ATTR_COLD void VMain___024root___settle__TOP__0(VMain___024root* vlSelf) {
     if (false && vlSelf) {}  // Prevent unused
     VMain__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
     VL_DEBUG_IF(VL_DBG_MSGF("+    VMain___024root___settle__TOP__0\n"); );
+    // Init
+    IData/*31:0*/ __Vtask_ebreak__0__Vfuncout;
     // Body
-    VMain___024unit____Vdpiimwrap_ebreak_TOP____024unit(vlSelf->__Vfunc_ebreak__0__Vfuncout);
-    vlSelf->Main__DOT__contr__DOT__ebreakbox__DOT__tmp 
-        = ((0x100073U == vlSelf->io_Inst) ? vlSelf->__Vfunc_ebreak__0__Vfuncout
-            : 0U);
+    if ((0x100073U == vlSelf->io_Inst)) {
+        VMain___024unit____Vdpiimwrap_ebreak_TOP____024unit(__Vtask_ebreak__0__Vfuncout);
+    }
     vlSelf->io_AluOp = ((0x13U == (0x7fU & vlSelf->io_Inst))
                          ? 1U : 0U);
     vlSelf->io_DataImmI = (QData)((IData)((vlSelf->io_Inst 
--- npc/vsrc/EbreakBox.v
+++ npc/vsrc/EbreakBox.v
@@ -1,5 +1,5 @@
 
- import "DPI-C" function int ebreak();
+ import "DPI-C" function void ebreak();
  module EbreakBox (ebreak_in);
  input ebreak_in;
  always@(*)
