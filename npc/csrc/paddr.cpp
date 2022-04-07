8848a20940cc7c0fca1f12888f0014cfbbe75bdd
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-07 13:20:07 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:20:07 up 3 days, 22:48,  3 users,  load average: 0.15, 0.21, 0.12

--- npc/csrc/paddr.cpp
+++ npc/csrc/paddr.cpp
@@ -7,7 +7,7 @@ typedef unsigned long long word_t;
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
-typedef unsigned long long uint64_t;
+typedef unsigned long long uint64_t1;
 
 static uint8_t pmem[CONFIG_MSIZE]= {0};
 
@@ -19,7 +19,7 @@ static inline word_t host_read(void *addr, int len) {
     case 1: return *(uint8_t  *)addr;
     case 2: return *(uint16_t *)addr;
     case 4: return *(uint32_t *)addr;
-    case 8: return *(uint64_t *)addr;
+    case 8: return *(uint64_t1 *)addr;
   }
   return 0;
 }
@@ -29,7 +29,7 @@ static inline void host_write(void *addr, int len, word_t data) {
     case 1: *(uint8_t  *)addr = data; return;
     case 2: *(uint16_t *)addr = data; return;
     case 4: *(uint32_t *)addr = data; return;
-    case 8: *(uint64_t *)addr = data; return;
+    case 8: *(uint64_t1 *)addr = data; return;
   }
 }
 
