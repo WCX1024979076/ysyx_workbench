240571b448de103295891b1fb38051f954ddd50d
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-07 14:34:55 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:34:55 up 4 days, 2 min,  4 users,  load average: 0.05, 0.06, 0.08

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -33,7 +33,7 @@ void ebreak()
 void pmem_read(long long Raddr, long long *Rdata) {
     (*Rdata) = *((uint32_t *)guest_to_host(Raddr));
     //printf("%lld\n",Raddr);
-    (*Rdata)=0x00100073;
+    //(*Rdata)=0x00100073;
     return ;
 }
 
