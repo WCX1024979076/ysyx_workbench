a2753c3af0c5cd20bfaaf30a80c99d7e2d75975f
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-07 13:19:15 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:19:15 up 3 days, 22:47,  3 users,  load average: 0.29, 0.24, 0.13

--- npc/csrc/paddr.cpp
+++ npc/csrc/paddr.cpp
@@ -7,7 +7,7 @@ typedef unsigned long long word_t;
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
-//typedef long long uint64_t;
+typedef unsigned long long uint64_t;
 
 static uint8_t pmem[CONFIG_MSIZE]= {0};
 
