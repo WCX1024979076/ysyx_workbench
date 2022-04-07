428fbfebe440bfa57090381954f3d5e8a95b439b
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-07 13:32:26 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:32:26 up 3 days, 23:00,  3 users,  load average: 0.41, 0.24, 0.15

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -9,37 +9,10 @@
 #define CONFIG_MSIZE 0x0000200
 #define CONFIG_MBASE 0x80000000
 
-typedef unsigned long int paddr_t;
-typedef unsigned long long word_t;
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long long uint64_tt;
-
 static uint8_t pmem[CONFIG_MSIZE]= {0};
 
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
-
-static inline word_t host_read(void *addr, int len) {
-  switch (len) {
-    case 1: return *(uint8_t  *)addr;
-    case 2: return *(uint16_t *)addr;
-    case 4: return *(uint32_t *)addr;
-    case 8: return *(uint64_tt *)addr;
-  }
-  return 0;
-}
-
-static inline void host_write(void *addr, int len, word_t data) {
-  switch (len) {
-    case 1: *(uint8_t  *)addr = data; return;
-    case 2: *(uint16_t *)addr = data; return;
-    case 4: *(uint32_t *)addr = data; return;
-    case 8: *(uint64_tt *)addr = data; return;
-  }
-}
-
 
 
 #define MAX_SIM_TIME 20
@@ -58,7 +31,7 @@ void ebreak()
 }
 
 void pmem_read(long long Raddr, long long *Rdata) {
-    (*Rdata) = host_read(guest_to_host(Raddr), 4);
+    (*Rdata) = *((uint32_t *)guest_to_host(Raddr));
     return ;
 }
 
