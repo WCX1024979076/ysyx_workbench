52927a3787fb46302cf6fa59bdc5aad6ae3bdc84
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-08 15:11:39 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:11:39 up 5 days, 39 min,  5 users,  load average: 0.92, 0.49, 0.30

--- npc/vsrc/Mem.v
+++ npc/vsrc/Mem.v
@@ -3,7 +3,7 @@ import "DPI-C" function void pmem_read(
   input longint Raddr, output longint Rdata);
 import "DPI-C" function void pmem_write(
   input longint Waddr, input longint Wdata, input byte Wmask);
-module Mem (Raddr,Rdata,Waddr,Wdata,Wmask,MemWrite,MemRead);
+module Mem (Raddr,Rdata,Waddr,Wdata,Wmask,MemWrite);
  input [63:0] Raddr,Waddr,Wdata;
  input [7:0] Wmask;
  input MemWrite;
