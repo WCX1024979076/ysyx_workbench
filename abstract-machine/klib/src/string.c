f602ec8d872b8c85a6e13bbc5d127bf28bdac4cf
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-09 20:02:26 +0800

    >  compile NEMU ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:02:26 up 6 days,  5:30,  5 users,  load average: 0.12, 0.09, 0.02

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -25,14 +25,14 @@ int strcmp(const char *s1, const char *s2) {
   for(i=0;s1[i]&&s2[i];i++)
   {
     if(s1[i]<s2[i])
-      return 0;
+      return -1;
     else if(s1[i]>s2[i])
-      return 0;
+      return 1;
   }
   if(!s1[i]&&s2[i])
-    return 0;
+    return -1;
   if(s1[i]&&!s2[i])
-    return 0;
+    return 1;
   return 0;
   //panic("Not implemented");
 }
