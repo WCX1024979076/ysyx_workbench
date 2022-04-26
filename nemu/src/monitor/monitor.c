dc681d92ff24dd5508409d49f5ae9760e92e5be7
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-26 11:02:00 +0800

    >  compile NEMU ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:02:00 up 14 days, 11:38,  3 users,  load average: 0.35, 0.41, 0.38

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -8,6 +8,7 @@ void init_difftest(char *ref_so_file, long img_size, int port);
 void init_device();
 void init_sdb();
 void init_disasm(const char *triple);
+void read_elf(char *elf_name);
 
 static void welcome() {
   Log("Trace: %s", MUXDEF(CONFIG_TRACE, ASNI_FMT("ON", ASNI_FG_GREEN), ASNI_FMT("OFF", ASNI_FG_RED)));
@@ -29,6 +30,7 @@ void sdb_set_batch_mode();
 static char *log_file = NULL;
 static char *diff_so_file = NULL;
 static char *img_file = NULL;
+static char *elf_file = NULL;
 static int difftest_port = 1234;
 
 static long load_img() {
@@ -69,6 +71,7 @@ static int parse_args(int argc, char *argv[]) {
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
+      case 'e': elf_file = optarg; break;
       case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
@@ -76,6 +79,7 @@ static int parse_args(int argc, char *argv[]) {
         printf("\t-l,--log=FILE           output log to FILE\n");
         printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+        printf("\t-e,--elf=FILE          input elf file\n");
         printf("\n");
         exit(0);
     }
@@ -106,6 +110,9 @@ void init_monitor(int argc, char *argv[]) {
 
   /* Load the image to memory. This will overwrite the built-in image. */
   long img_size = load_img();
+
+  /* read elf file*/
+  read_elf(elf_file);
 
   /* Initialize differential testing. */
   init_difftest(diff_so_file, img_size, difftest_port);
---
+++ nemu/src/utils/elf.c
@@ -0,0 +1,64 @@
+#include <elf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <common.h>
+
+typedef struct
+{
+    uint64_t fun_addr;
+    uint64_t func_size;
+    char *fun_name;
+} elf_info;
+elf_info elf_func[1000];
+int elf_cnt = 0;
+
+void read_elf(char *elf_name)
+{
+    if (elf_name == NULL)
+    {
+        Log("no input elf file\n");
+        return;
+    }
+    FILE *stream;
+    stream = fopen(elf_name, "rb");
+    assert(stream);
+    unsigned char *buffer;
+    buffer = (unsigned char *)malloc(100500 * sizeof(unsigned char));
+    int ret = fread(buffer, sizeof(unsigned char), 100500, stream);
+    assert(ret == 1);
+    Elf64_Ehdr *ehdr = (Elf64_Ehdr *)buffer;
+    Elf64_Shdr *shdr = (Elf64_Shdr *)(buffer + ehdr->e_shoff);
+    Elf64_Shdr *shdr_strtab = NULL;
+    Elf64_Shdr *shdr_symtab = NULL;
+
+    for (int i = 0; shdr_strtab == NULL || shdr_symtab == NULL; i++)
+    {
+        if (shdr[i].sh_type == SHT_SYMTAB)
+        {
+            shdr_symtab = &shdr[i];
+        }
+        else if (shdr[i].sh_type == SHT_STRTAB)
+        {
+            shdr_strtab = &shdr[i];
+            break;
+        }
+    }
+
+    Elf64_Sym *table_sym = (Elf64_Sym *)(buffer + shdr_symtab->sh_offset);
+
+    for (int i = 0; i <= shdr_symtab->sh_size / shdr_symtab->sh_entsize; i++)
+    {
+        if (ELF64_ST_TYPE(table_sym[i].st_info) == STT_FUNC)
+        {
+            elf_func[elf_cnt].fun_addr = table_sym[i].st_value;
+            elf_func[elf_cnt].func_size = table_sym[i].st_size;
+            elf_func[elf_cnt].fun_name = (char *)(buffer + shdr_strtab->sh_offset + table_sym[i].st_name);
+            elf_cnt++;
+        }
+    }
+
+    for (int i = 0; i < elf_cnt; i++)
+        Log("%lx %lx %s\n", elf_func[i].fun_addr, elf_func[i].func_size, elf_func[i].fun_name);
+    return;
+}
