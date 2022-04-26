745f1694d6a00fc68a822211f01335e395ce188d
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-26 13:51:44 +0800

    >  compile NEMU ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:51:44 up 14 days, 14:28,  3 users,  load average: 0.32, 0.32, 0.34

--- nemu/src/utils/elf.c
+++ nemu/src/utils/elf.c
@@ -63,6 +63,6 @@ void read_elf(char *elf_name)
     }
 
     for (int i = 0; i < elf_cnt; i++)
-        Log("%lx %lx %s\n", elf_func[i].fun_addr, elf_func[i].func_size, elf_func[i].fun_name);
+        Log("%lx %lx %s", elf_func[i].fun_addr, elf_func[i].func_size, elf_func[i].fun_name);
     return;
 }
