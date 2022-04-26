ba9885b9613fc96bbe2db1868586f635d2530b00
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-26 23:28:02 +0800

    >  compile NEMU ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:28:02 up 15 days, 4 min,  2 users,  load average: 0.05, 0.28, 0.40

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,56 +4,71 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
+size_t strlen(const char *s)
+{
+  size_t len = 0;
+  while (s[len])
+    len++;
+  return len;
+}
+
+char *strcpy(char *dst, const char *src)
+{
   panic("Not implemented");
 }
 
-char *strcpy(char *dst, const char *src) {
+char *strncpy(char *dst, const char *src, size_t n)
+{
   panic("Not implemented");
 }
 
-char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+char *strcat(char *dst, const char *src)
+{
+  size_t dst_len = strlen(dst);
+  for (int i = 0; src[i]; i++)
+    dst[dst_len + i] = src[i];
+  return dst;
 }
 
-char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
-}
-
-int strcmp(const char *s1, const char *s2) {
-  int i=0;
-  for(i=0;s1[i]&&s2[i];i++)
+int strcmp(const char *s1, const char *s2)
+{
+  int i = 0;
+  for (i = 0; s1[i] && s2[i]; i++)
   {
-    if(s1[i]<s2[i])
+    if (s1[i] < s2[i])
       return -1;
-    else if(s1[i]>s2[i])
+    else if (s1[i] > s2[i])
       return 1;
   }
-  if(!s1[i]&&s2[i])
+  if (!s1[i] && s2[i])
     return -1;
-  if(s1[i]&&!s2[i])
+  if (s1[i] && !s2[i])
     return 1;
   return 0;
-  //panic("Not implemented");
 }
 
-int strncmp(const char *s1, const char *s2, size_t n) {
+int strncmp(const char *s1, const char *s2, size_t n)
+{
   panic("Not implemented");
 }
 
-void *memset(void *s, int c, size_t n) {
+void *memset(void *s, int c, size_t n)
+{
   panic("Not implemented");
 }
 
-void *memmove(void *dst, const void *src, size_t n) {
+void *memmove(void *dst, const void *src, size_t n)
+{
   panic("Not implemented");
 }
 
-void *memcpy(void *out, const void *in, size_t n) {
+void *memcpy(void *out, const void *in, size_t n)
+{
   panic("Not implemented");
 }
 
-int memcmp(const void *s1, const void *s2, size_t n) {
+int memcmp(const void *s1, const void *s2, size_t n)
+{
   panic("Not implemented");
 }
 
