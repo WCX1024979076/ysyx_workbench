ae5ece89998433b7e3b7d4720cee5f23f32913ff
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-27 13:54:17 +0800

    >  compile NEMU ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:54:17 up 15 days, 14:31,  2 users,  load average: 0.02, 0.03, 0.00

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -70,7 +70,7 @@ void *memset(void *s, int c, size_t n)
     return NULL;
 
   void *tmp = s;
-  unsigned char *str = s;
+  char *str = s;
   for (int i = 0; i < n; i++)
     str[i] = c;
   return tmp;
@@ -81,24 +81,18 @@ void *memmove(void *dst, const void *src, size_t n)
   if (dst == NULL || n < 0 || src == NULL)
     return NULL;
 
-  void *ret = dst;
-  unsigned char *str_dst = dst;
-  const unsigned char *str_src = src;
+  char *str_dst = dst;
+  const char *str_src = src;
   if (str_dst > str_src && str_src + n > str_dst)
   {
-    str_dst = str_dst + n - 1;
-    str_src = str_src + n - 1;
-    while (n--)
-    {
-      *str_dst = *str_src;
-      str_dst--, str_src--;
-    }
+    for (int i = n - 1; i >= 0; i--)
+      str_dst[i] = str_src[i];
   }
   else
   {
     memcpy(dst, src, n);
   }
-  return ret;
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n)
@@ -106,23 +100,19 @@ void *memcpy(void *out, const void *in, size_t n)
   if (out == NULL || n < 0 || in == NULL)
     return NULL;
 
-  void *ret = out;
-  unsigned char *str_out = out;
-  const unsigned char *str_in = in;
-  while (n--)
-  {
-    *str_out = *str_in;
-    str_out++, str_in++;
-  }
-  return ret;
+  char *str_out = out;
+  const char *str_in = in;
+  for (int i = 0; i < n; i++)
+    str_out[i] = str_in[i];
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n)
 {
   if (s1 == NULL || n < 0 || s2 == NULL)
     return -1;
-  const unsigned char *str_s1 = s1;
-  const unsigned char *str_s2 = s2;
+  const char *str_s1 = s1;
+  const char *str_s2 = s2;
 
   for (int i = 0; i < n; i++)
   {
