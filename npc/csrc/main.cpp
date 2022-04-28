c3e157eee3cd1fb9442ade2570acdcbb9028fbb4
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-28 20:27:17 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:27:17 up 16 days, 21:04,  5 users,  load average: 0.13, 0.12, 0.07

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -197,6 +197,8 @@ int main(int argc, char **argv, char **env)
     ref_difftest_regcpy(ref_cpu, DIFFTEST_TO_DUT);
     // printf("123");
     printf("123");
+        assert(0);
+
     for (int i = 0; i < 32; i++)
     {
       if (cpu.gpr[i] != ref_cpu->gpr[i])
