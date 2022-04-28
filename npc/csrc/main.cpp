85558cd93dee91c988f11b7be282d0e625426eac
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-28 18:35:45 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:35:45 up 16 days, 19:12,  4 users,  load average: 0.00, 0.01, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -7,15 +7,15 @@
 #include "VMain__Dpi.h"
 #include "verilated_dpi.h"
 
-// void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction);
-// void difftest_regcpy(void *dut, bool direction);
-// void difftest_exec(uint64_t n);
-// void difftest_raise_intr(word_t NO);
-void difftest_init();
+void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
+void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
+void (*ref_difftest_exec)(uint64_t n) = NULL;
+void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
 
 #define CONFIG_MSIZE 0x0020000
 #define CONFIG_MBASE 0x80000000
-
+#define CONFIG_PC_RESET_OFFSET 0x0
+#define RESET_VECTOR (CONFIG_MBASE + CONFIG_PC_RESET_OFFSET)
 static uint8_t pmem[CONFIG_MSIZE] = {0};
 
 uint8_t *guest_to_host(long long paddr) { return pmem + paddr - CONFIG_MBASE; }
@@ -114,6 +114,40 @@ long ld(char *img_file)
     printf("%llx %016llx\n",i,tmp);
   }
   */
+}
+
+void init_so(char *ref_so_file, long img_size)
+{
+  assert(ref_so_file != NULL);
+
+  void *handle;
+  handle = dlopen(ref_so_file, RTLD_LAZY);
+  assert(handle);
+
+  ref_difftest_memcpy = dlsym(handle, "difftest_memcpy");
+  assert(ref_difftest_memcpy);
+
+  ref_difftest_regcpy = dlsym(handle, "difftest_regcpy");
+  assert(ref_difftest_regcpy);
+
+  ref_difftest_exec = dlsym(handle, "difftest_exec");
+  assert(ref_difftest_exec);
+
+  ref_difftest_raise_intr = dlsym(handle, "difftest_raise_intr");
+  assert(ref_difftest_raise_intr);
+
+  void (*ref_difftest_init)(int) = dlsym(handle, "difftest_init");
+  assert(ref_difftest_init);
+
+  
+  Log("Differential testing: %s", ASNI_FMT("ON", ASNI_FG_GREEN));
+  Log("The result of every instruction will be compared with %s. "
+      "This will help you a lot for debugging, but also significantly reduce the performance. "
+      "If it is not necessary, you can turn it off in menuconfig.", ref_so_file);
+
+  ref_difftest_init();
+  ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), img_size, DIFFTEST_TO_REF);
+  ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
 }
 int main(int argc, char **argv, char **env)
 {
