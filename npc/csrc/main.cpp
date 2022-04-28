19c67effdc94e92745aac0707e0376834afee962
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-28 23:50:54 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:50:54 up 17 days, 27 min,  6 users,  load average: 0.01, 0.09, 0.14

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -52,7 +52,7 @@ long long host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 void ebreak()
 {
   puts("Meet ebreak;");
-  puts("%lx\n",cpu_npc.gpr[10]);
+  printf("%lx\n",cpu_npc.gpr[10]);
   if (cpu_npc.gpr[10] == 1)
     exit(-1);
   else
