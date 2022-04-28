5b56e75cb5566a727352b697fabe2df0262a7fb2
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-28 18:36:51 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:36:51 up 16 days, 19:13,  4 users,  load average: 0.00, 0.00, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include "VMain__Dpi.h"
 #include "verilated_dpi.h"
+#include <dlfcn.h>
 
 void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
