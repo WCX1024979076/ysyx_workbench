dc5e555b436870315b0cf63efac5ea349f070199
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-28 20:30:48 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:30:48 up 16 days, 21:07,  5 users,  load average: 0.10, 0.11, 0.08

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -164,7 +164,7 @@ void check_regs_npc(CPU_state *ref_cpu)
   }
   if (cpu_npc.pc != ref_cpu->pc)
   {
-    printf("Missing match at pc, npc_val=%lx,nemu_val=%lx", cpu.pc, ref_cpu->pc);
+    printf("Missing match at pc, npc_val=%lx,nemu_val=%lx", cpu_npc.pc, ref_cpu->pc);
     exit(0);
   }
 }
