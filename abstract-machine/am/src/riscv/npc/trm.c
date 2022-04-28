6872733a80336608fb8db8e7e27e59d5023a087f
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-28 08:17:28 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:17:28 up 16 days,  8:54,  1 user,  load average: 0.12, 0.09, 0.02

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -6,7 +6,7 @@ int main(const char *args);
 
 extern char _pmem_start;
 #define PMEM_SIZE (128 * 1024 * 1024)
-#define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
+#define PMEM_END ((uintptr_t)&_pmem_start + PMEM_SIZE)
 
 Area heap = RANGE(&_heap_start, PMEM_END);
 #ifndef MAINARGS
@@ -14,19 +14,23 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 #endif
 static const char mainargs[] = MAINARGS;
 
-void putch(char ch) {
+void putch(char ch)
+{
 }
 
-void halt(int code) {
+void halt(int code)
+{
+  code = !code;
   asm volatile(
-    "mv a7,%0\n"
-    "ebreak"
-    : "=r"(code)
-  );
-  while (1);
+      "mv a7,%0\n"
+      "ebreak"
+      : "=r"(code));
+  while (1)
+    ;
 }
 
-void _trm_init() {
+void _trm_init()
+{
   int ret = main(mainargs);
   halt(ret);
 }
