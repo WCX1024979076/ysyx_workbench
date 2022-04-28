1fc971262412d941785749510e7c82eacdd7774e
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-28 18:52:00 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:52:00 up 16 days, 19:28,  4 users,  load average: 0.06, 0.02, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -13,7 +13,7 @@ void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) =
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
 void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
-void (*ref_difftest_init)(uint64_t NO) = NULL;
+void (*ref_difftest_init)() = NULL;
 
 #define CONFIG_MSIZE 0x0020000
 #define CONFIG_MBASE 0x80000000
