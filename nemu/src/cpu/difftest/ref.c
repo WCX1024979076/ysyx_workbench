c4acde2a935b00ca60b9d2c7c37c905e5021df80
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-28 19:35:25 +0800

    >  compile NEMU ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:35:25 up 16 days, 20:12,  5 users,  load average: 0.08, 0.10, 0.04

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -3,37 +3,43 @@
 #include <difftest-def.h>
 #include <memory/paddr.h>
 
-void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  if(direction == DIFFTEST_TO_DUT)
+void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
+{
+  if (direction == DIFFTEST_TO_DUT)
   {
-    buf=(void*)guest_to_host(addr);
+    buf = (void *)guest_to_host(addr);
   }
-  else if(direction == DIFFTEST_TO_REF)
+  else if (direction == DIFFTEST_TO_REF)
   {
-    paddr_write(addr,n,*(word_t*)buf);
+    Log("%x,%lx", addr, n);
+    paddr_write(addr, n, *(word_t *)buf);
   }
 }
 
-void difftest_regcpy(void *dut, bool direction) {
-  if(direction == DIFFTEST_TO_DUT)
+void difftest_regcpy(void *dut, bool direction)
+{
+  if (direction == DIFFTEST_TO_DUT)
   {
     dut = &cpu;
   }
-  else if(direction == DIFFTEST_TO_REF)
+  else if (direction == DIFFTEST_TO_REF)
   {
-    cpu = *(CPU_state*)dut;
+    cpu = *(CPU_state *)dut;
   }
 }
 
-void difftest_exec(uint64_t n) {
+void difftest_exec(uint64_t n)
+{
   cpu_exec(n);
 }
 
-void difftest_raise_intr(word_t NO) {
+void difftest_raise_intr(word_t NO)
+{
   assert(0);
 }
 
-void difftest_init(/*int port*/) {
+void difftest_init(/*int port*/)
+{
   /* Perform ISA dependent initialization. */
   init_isa();
   void init_rand();
@@ -41,4 +47,3 @@ void difftest_init(/*int port*/) {
   init_rand();
   init_mem();
 }
-
