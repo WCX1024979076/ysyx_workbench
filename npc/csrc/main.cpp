dd5d6008aec9f8275fe536b11f9bdb0ad74da94f
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-28 18:38:47 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:38:47 up 16 days, 19:15,  4 users,  load average: 0.00, 0.00, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -8,6 +8,7 @@
 #include "verilated_dpi.h"
 #include <dlfcn.h>
 
+typedef uint32_t paddr_t;
 void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
@@ -154,7 +155,7 @@ int main(int argc, char **argv, char **env)
       size = ld(argv[1]);
     }
   }
-  
+
   srand(time(0));
   contextp = new VerilatedContext;
   contextp->commandArgs(argc, argv);
