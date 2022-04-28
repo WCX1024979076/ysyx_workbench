acd90259d14acd518d9653d4af5b5aa2922efe24
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-28 19:50:16 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:50:16 up 16 days, 20:27,  5 users,  load average: 0.03, 0.02, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -9,6 +9,10 @@
 #include <dlfcn.h>
 typedef uint64_t word_t;
 typedef word_t vaddr_t;
+#define CONFIG_MSIZE 0x0020000
+#define CONFIG_MBASE 0x80000000
+#define CONFIG_PC_RESET_OFFSET 0x0
+#define RESET_VECTOR (CONFIG_MBASE + CONFIG_PC_RESET_OFFSET)
 
 enum
 {
@@ -22,6 +26,15 @@ typedef struct
 } CPU_state;
 
 CPU_state cpu;
+CPU_state ref_cpu;
+
+uint64_t *cpu_gpr = NULL;
+
+vluint64_t sim_time = 0;
+VMain *top = nullptr;
+VerilatedVcdC *m_trace = nullptr;
+VerilatedContext *contextp = nullptr;
+
 typedef uint32_t paddr_t;
 void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
@@ -29,19 +42,11 @@ void (*ref_difftest_exec)(uint64_t n) = NULL;
 void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
 void (*ref_difftest_init)() = NULL;
 
-#define CONFIG_MSIZE 0x0020000
-#define CONFIG_MBASE 0x80000000
-#define CONFIG_PC_RESET_OFFSET 0x0
-#define RESET_VECTOR (CONFIG_MBASE + CONFIG_PC_RESET_OFFSET)
 static uint8_t pmem[CONFIG_MSIZE] = {0};
 
 uint8_t *guest_to_host(long long paddr) { return pmem + paddr - CONFIG_MBASE; }
 long long host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
-vluint64_t sim_time = 0;
-VMain *top = nullptr;
-VerilatedVcdC *m_trace = nullptr;
-VerilatedContext *contextp = nullptr;
 void ebreak()
 {
   puts("Meet ebreak;");
@@ -57,19 +62,12 @@ void ebreak()
   exit(0);
 }
 
-uint64_t *cpu_gpr = NULL;
 extern "C" void set_gpr_ptr(const svOpenArrayHandle r)
 {
   cpu_gpr = (uint64_t *)(((VerilatedDpiOpenVar *)r)->datap());
-}
-
-void dump_gpr()
-{
-  int i;
-  for (i = 0; i < 33; i++)
-  {
-    printf("gpr[%d] = 0x%lx\n", i, cpu_gpr[i]);
-  }
+  for (int i = 0; i < 32; i++)
+    cpu.gpr[i] = cpu_gpr[i];
+  cpu.pc = cpu_gpr[32];
 }
 
 void pmem_read(long long Raddr, long long *Rdata)
@@ -158,20 +156,24 @@ void init_so(char *ref_so_file, long img_size)
   ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
 }
 
-int main(int argc, char **argv, char **env)
+void check_regs()
 {
-  long size = 0;
-  if (argc == 2)
+  for (int i = 0; i < 32; i++)
   {
-    if (strlen(argv[1]) != 0)
+    if (cpu.gpr[i] != ref_cpu.gpr[i])
     {
-      printf("ld:%s\n", argv[1]);
-      size = ld(argv[1]);
+      printf("Missing match at reg%d, npc_val=%lx,nemu_val=%lx", i, cpu.gpr[i], ref_cpu.gpr[i]);
+      exit(0);
     }
   }
-  init_so("/home/wcx/Desktop/ysyx-workbench/nemu/build/riscv64-nemu-interpreter-so", size);
-  return 0;
-  srand(time(0));
+  if (cpu.pc != ref_cpu.pc)
+  {
+    printf("Missing match at pc, npc_val=%lx,nemu_val=%lx", cpu.gpr[i], ref_cpu.gpr[i]);
+    exit(0);
+  }
+}
+void init_npc()
+{
   contextp = new VerilatedContext;
   contextp->commandArgs(argc, argv);
   top = new VMain{contextp};
@@ -186,10 +188,28 @@ int main(int argc, char **argv, char **env)
   for (int i = 1; i <= 10; i++)
     cpu_sim();
   top->reset = 0;
+}
+
+int main(int argc, char **argv, char **env)
+{
+  long size = 0;
+  if (argc == 2)
+  {
+    if (strlen(argv[1]) != 0)
+    {
+      printf("ld:%s\n", argv[1]);
+      size = ld(argv[1]);
+    }
+  }
+  init_npc();
+  init_so("/home/wcx/Desktop/ysyx-workbench/nemu/build/riscv64-nemu-interpreter-so", size);
   while (1)
   {
     m_trace->dump(sim_time++);
     cpu_sim();
+    ref_difftest_exec(1);
+    ref_difftest_regcpy(&ref_cpu, DIFFTEST_TO_DUT);
+    check_regs();
   }
 
   m_trace->close();
