18d86d6f4c25152b49a977b3ecb2d6e9fa645341
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-28 20:17:29 +0800

    >  compile NEMU ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:17:29 up 16 days, 20:54,  5 users,  load average: 0.09, 0.09, 0.03

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -24,9 +24,11 @@ void difftest_regcpy(void *dut, bool direction)
 {
   if (direction == DIFFTEST_TO_DUT)
   {
+    printf("123");
+
     dut = &cpu;
     // printf("nemu pc %lx\n", ((CPU_state *)dut)->pc);
-    printf("123");
+    // printf("123");
   }
   else if (direction == DIFFTEST_TO_REF)
   {
