8cf2d2d8251b69a995a91ae87550e4e7eef24652
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-29 11:33:30 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:33:30 up 17 days, 12:10,  6 users,  load average: 0.25, 0.21, 0.14

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -187,7 +187,7 @@ int main(int argc, char **argv, char **env)
   {
     if (strlen(argv[1]) != 0)
     {
-      printf("ld1:%s\n", argv[1]);
+      printf("ld:%s\n", argv[1]);
       printf("123");
       size = ld(argv[1]);
     }
