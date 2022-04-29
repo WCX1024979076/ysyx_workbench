ff2bd5afa9bc3dbafe177f94be4ccf712c25aeb3
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-29 11:36:47 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:36:47 up 17 days, 12:13,  6 users,  load average: 0.15, 0.16, 0.12

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -196,7 +196,7 @@ int main(int argc, char **argv, char **env)
   contextp->commandArgs(argc, argv);
   top = new VMain{contextp};
 
-  Verilated::traceEverOn(true);
+  // Verilated::traceEverOn(true);
 
   // m_trace = new VerilatedVcdC;
   // top->trace(m_trace, 5);
