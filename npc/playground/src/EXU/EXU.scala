3125271c7715e5223d8b2a8af05a1384c37156b0
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-29 13:15:21 +0800

    >  generate verilog ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:15:21 up 17 days, 13:52,  6 users,  load average: 0.57, 0.27, 0.10

--- npc/playground/src/EXU/EXU.scala
+++ npc/playground/src/EXU/EXU.scala
@@ -77,7 +77,7 @@ class EXU extends Module {
     0xd.U -> (DataR1 ^ DataR2),                                             //xor
     0xe.U -> (DataR1 ^ Cat(Fill(52, io.Imm(11)), io.Imm(11,0))),            //xori
     0xf.U -> (DataR1 | DataR2),                                             //or
-    0x010.U -> (DataR1 | Cat(Fill(52, io.Imm(11)), io.Imm(11,0))),           //ori
+    "h10".U(5.W) -> (DataR1 | Cat(Fill(52, io.Imm(11)), io.Imm(11,0))),           //ori
   ));
 
   io.PcVal := pc;
