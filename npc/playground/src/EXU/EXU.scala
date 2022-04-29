9c35abd464c741ee3db97085c0c955677ed6010f
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-29 13:22:52 +0800

    >  generate verilog ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:22:52 up 17 days, 13:59,  6 users,  load average: 0.49, 0.59, 0.32

--- npc/playground/src/EXU/EXU.scala
+++ npc/playground/src/EXU/EXU.scala
@@ -71,12 +71,12 @@ class EXU extends Module {
     0x7.U -> (DataR1 =/= DataR2),                                           //bne
     0x8.U -> (DataR1 - DataR2),                                             //sub
     0x9.U -> (DataR1 << DataR2(5,0)),                                       //sll,slli
-    0xa.U -> (DataR1 >> DataR2(5,0)),                                       //srl,srli
-    0xb.U -> (DataR1.asSInt() >> DataR2(5,0)),                              //sra,srai
-    0xc.U -> (io.Imm(31,12) << 12.U),                                       //lui
-    0xd.U -> (DataR1 ^ DataR2),                                             //xor
-    0xe.U -> (DataR1 ^ Cat(Fill(52, io.Imm(11)), io.Imm(11,0))),            //xori
-    0xf.U -> (DataR1 | DataR2),                                             //or
+    //0xa.U -> (DataR1 >> DataR2(5,0)),                                       //srl,srli
+    //0xb.U -> (DataR1.asSInt() >> DataR2(5,0)),                              //sra,srai
+    //0xc.U -> (io.Imm(31,12) << 12.U),                                       //lui
+    //0xd.U -> (DataR1 ^ DataR2),                                             //xor
+    //0xe.U -> (DataR1 ^ Cat(Fill(52, io.Imm(11)), io.Imm(11,0))),            //xori
+    //0xf.U -> (DataR1 | DataR2),                                             //or
     //0x10.U -> (DataR1 | Cat(Fill(52, io.Imm(11)), io.Imm(11,0))),           //ori
   ));
 
