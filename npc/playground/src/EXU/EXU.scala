07d832819b87b15d1529d0510a1425787030c179
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-29 21:37:24 +0800

    >  generate verilog ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:37:24 up 17 days, 22:14,  6 users,  load average: 0.29, 0.17, 0.07

--- npc/playground/src/EXU/EXU.scala
+++ npc/playground/src/EXU/EXU.scala
@@ -68,9 +68,9 @@ class EXU extends Module {
   ));
 
   var AluSrc1 := MuxLookup(io.AluSrc1Op,DataR2,Array(    
-    "b00000".U -> DataR1,
-    "b00001".U -> pc,
-    "b00010".U -> io.Imm(31,12),
+    0x0.U -> DataR1,
+    0x1.U -> pc,
+    0x2.U -> io.Imm(31,12),
   ));
 
   var AluSrc2 := MuxLookup(io.AluSrc2Op,DataR2,Array(
