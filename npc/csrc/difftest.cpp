b17d72314ed36dbd3f1eaf3252d88152274a9b49
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-02 16:23:34 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:23:34 up 20 days, 17:00,  3 users,  load average: 0.06, 0.09, 0.09

--- npc/csrc/difftest.cpp
+++ npc/csrc/difftest.cpp
@@ -1,6 +1,11 @@
 #include "npc.h"
 
 #ifdef CONFIG_DIFFTEST
+void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
+void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
+void (*ref_difftest_exec)(uint64_t n) = NULL;
+void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
+void (*ref_difftest_init)() = NULL;
 void init_so(char *ref_so_file, long img_size)
 {
   assert(ref_so_file != NULL);
