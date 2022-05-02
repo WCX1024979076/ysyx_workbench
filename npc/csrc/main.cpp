77e2b65770fa246201df7095bca89ac916493da8
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-02 15:57:29 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:57:29 up 20 days, 16:34,  3 users,  load average: 1.15, 0.48, 0.18

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -87,7 +87,7 @@ int main(int argc, char **argv, char **env)
       size = ld(argv[1]);
     }
   }
-  printf("123");
+  puts("123");
 
   contextp = new VerilatedContext;
   contextp->commandArgs(argc, argv);
