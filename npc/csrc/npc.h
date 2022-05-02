ce992251f102f9bd8df7fa1638255bda2caf8563
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-02 16:14:24 +0800

    >  sim RTL ysyx_22040785 王春祥 Linux VM-24-9-ubuntu 5.4.0-99-generic #112-Ubuntu SMP Thu Feb 3 13:50:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:14:24 up 20 days, 16:51,  3 users,  load average: 0.06, 0.06, 0.09

--- npc/csrc/npc.h
+++ npc/csrc/npc.h
@@ -29,11 +29,11 @@ enum
   DIFFTEST_TO_REF
 };
 typedef uint32_t paddr_t;
-void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
-void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
-void (*ref_difftest_exec)(uint64_t n) = NULL;
-void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
-void (*ref_difftest_init)() = NULL;
+static void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
+static void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
+static void (*ref_difftest_exec)(uint64_t n) = NULL;
+static void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
+static void (*ref_difftest_init)() = NULL;
 #endif
 
 static uint8_t pmem[CONFIG_MSIZE] = {0};
